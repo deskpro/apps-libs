345ba48506c1f595c9ecb423454b3d70a7de1601
Radu Helstern <radu.helstern@gmail.com>
2018-08-15 16:55:16 +0100

    Cannot change app title (#4)
    * move the change title logic into the UI facade

--- packages/apps-sdk/test/unit/UI/render.test.js
+++ packages/apps-sdk/test/unit/UI/render.test.js
@@ -50,3 +50,44 @@ test('connectRenderer connects a renderer function', () => {
   expect(render.mock.calls[0][0]).toHaveProperty('expand', app.ui.expand);
   expect(render.mock.calls[0][0]).toHaveProperty('closeNotification', app.ui.closeNotification);
 });
+
+test('render function gets invoked with the correct app title', () => {
+
+  const app = createAppFromProps({
+    registerEventHandlers: function() {},
+    localDispatcher:    new AppEventEmitter(),
+    incomingDispatcher: new AppEventEmitter(),
+    outgoingDispatcher: new AppEventEmitter(),
+
+    instanceProps: {
+      appId: '1',
+      appTitle: 'title',
+      appPackageName: 'com.deskpro.app',
+      instanceId: '1',
+    },
+    contextProps: {
+      type: 'ticket',
+      entityId: '1',
+      locationId: '1',
+      tabId: 'tab-1',
+      tabUrl: 'https://127.0.0.1',
+    }
+  });
+
+  const render = jest.fn();
+  const renderFactory = dpapp => render;
+  const connectedRenderer = connectRenderer(app, renderFactory);
+
+  expect(typeof connectedRenderer).toEqual('function');
+
+  // trigger an update
+  app.ui.badgeCount = 1;
+  // trigger another update
+  app.ui.changeTitle('another title');
+  // trigger another update
+  app.ui.changeTitle()
+
+  expect(render.mock.calls[0][0]).toHaveProperty('title', 'title');
+  expect(render.mock.calls[1][0]).toHaveProperty('title', 'another title');
+  expect(render.mock.calls[2][0]).toHaveProperty('title', 'title');
+});
