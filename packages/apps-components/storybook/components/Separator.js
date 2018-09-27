875301caf27fab48a47df4dec0306a4e287d8768
Julien Ducro <julien.ducro@deskpro.com>
2018-09-27 12:31:05 +0100

    DP-2526 - Add Separator

---
+++ packages/apps-components/storybook/components/Separator.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+
+import { DataTable } from '../../src/Data';
+import { default as Tag } from '../../src/Tag';
+import { default as Panel } from '../../src/Panel';
+import { default as Separator } from '../../src/Separator';
+import { Action } from '../../src/Action';
+import {wrapInApp } from './decorators'
+
+
+const data = [
+  {
+    sku: 4356,
+    label: "Mens",
+    status: "On Hold"
+  },
+  {
+    sku: 4357,
+    label: "Washing Machine",
+    status: "Fulfilled"
+  },
+  {
+    sku: 4358,
+    label: "Television",
+    status: "Fulfilled"
+  },
+];
+
+const columns = [
+  function ({ sku }) {
+    return <span style={{ color: "#4696dc" }}>#{sku}</span>
+  },
+  "label",
+  function ({ status }) {
+    return <Tag color={status === 'On Hold' ? "blue" : "green"}>{status}</Tag>
+  }
+];
+
+storiesOf('Separator', module)
+  .add('normal' , wrapInApp([
+    <Panel title={"Products"}>
+      <Action icon="edit" label={"Edit"} labelDisplay={"onHover"} />
+      <Action icon="close-heavy" label={"Close"} labelDisplay={"onHover"} />
+      <DataTable columns={columns} data={data} />
+    </Panel>,
+    <Separator title="or" />,
+    <Panel title={"Products"}>
+      <Action icon="edit" label={"Edit"} labelDisplay={"onHover"} />
+    <Action icon="close-heavy" label={"Close"} labelDisplay={"onHover"} />
+    <DataTable columns={columns} data={data} />
+</Panel>
+  ]))
+;
